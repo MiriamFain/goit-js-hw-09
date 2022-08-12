a4d14e8961220f7239caffe61460f3d848b8ab82
Miriam Fainshtein <104686417+MiriamFain@users.noreply.github.com>
2022-08-12 19:11:13 +0300

    add tasks

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,43 +2,67 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const inputRef = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+startBtn.disabled = true;
+
+let counter = 0;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (Date.parse(selectedDates) < Date.parse(new Date())) {
+      Notify.failure('Please choose a date in the future');
+    } else {
+      counter = Date.parse(selectedDates[0]) - Date.parse(new Date());
+      startBtn.disabled = false;
+    }
   },
 };
 
-flatpickr(selector, options);
+startBtn.addEventListener('click', () => startHandler(counter));
 
-onClose();
+function startHandler() {
+  const showTimer = setInterval(() => {
+    const { days, hours, minutes, seconds } = convertMs(
+      (counter = counter - 1000)
+    );
+    const daysStr = days.toString();
+    const hoursStr = hours.toString();
+    const minutesStr = minutes.toString();
+    const secondsStr = seconds.toString();
+    daysRef.textContent =
+      daysStr.length > 1 ? daysStr : addLeadingZero(daysStr);
+    hoursRef.textContent =
+      hoursStr.length > 1 ? hoursStr : addLeadingZero(hoursStr);
+    minutesRef.textContent =
+      minutesStr.length > 1 ? minutesStr : addLeadingZero(minutesStr);
+    secondsRef.textContent =
+      secondsStr.length > 1 ? secondsStr : addLeadingZero(secondsStr);
+  }, 1000);
+}
 
-selectedDates;
-
-window.alert(`Please choose a date in the future.`);
+flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return value.padStart(2, '0');
+}
